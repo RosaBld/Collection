7ed954625126ba4e084283ceb6370c10f93ec986
Rosalie <rosalie.boulard@gmail.com>
2023-09-20 13:41:00 +0200

    let's get started

---
+++ script.js
@@ -0,0 +1,300 @@
+const collection = [
+    {
+        cover:"",
+        movie:"The BoogeyMan",
+        releaseYear:2023,
+        from:"United States",
+        producter:"Rob Savage",
+        cast:[
+            "Sophie Thatcher", 
+            "David Dastmalchian", 
+            "Vivien Lyra Blair", 
+            "Chris Messina", 
+            "Madison Hu", 
+            "Marin Ireland", 
+            "Lisa Gay Hamilton", 
+            "Maddie Nichols"
+        ],
+        summary:"",
+        sousGenres:"",
+        cotation:[
+            "IMDb:",
+            "RottenTomatoes:",
+            ""
+        ]
+    },
+    {
+        cover:"",
+        movie:"Crazy Bear",
+        releaseYear:2023,
+        from:"United States",
+        producter:"Elizabeth Banks",
+        cast:[
+            "Ray Liotta", 
+            "Keri Russell", 
+            "Jesse Tyler Ferguson", 
+            "Alden Ehrenreich", 
+            "Margot Martindale", 
+            "O'Shea Jackson Jr.", 
+            "Kristofer Hivju", 
+            "Scott Seiss"
+        ],
+        summary:"",
+        sousGenres:"",
+        cotation:[
+            "IMDb:",
+            "RottenTomatoes:",
+            ""
+        ]
+    },
+    {
+        cover:"",
+        movie:"Black Phone",
+        releaseYear: 2022,
+        from: "United States",
+        producer:"Scott Derrickson",
+        cast:[
+            "Ethan Hawke", 
+            "Mason Thames", 
+            "Miguel Cazarez", 
+            "Madelein McGraw", 
+            "Brady Hepner",
+            "Jeremy Davies", 
+            "Tristan Pravong", 
+            "Mike Bailey",
+            "etc"
+        ],
+        summary:"",
+        sousGenres:"",
+        cotation:[
+            "IMDb:",
+            "RottenTomatoes:",
+            ""
+        ]
+    },
+    {
+        movie:"Last train to Busan",
+        releaseYear:2016,
+        from: "South Korea",
+        producer:"Sang-Ho Yeon",
+        cast: [
+            "Gong Yoo", 
+            "Ma Dong-Seok", 
+            "Sang-ho Yeon", 
+            "Jung Yoo Mi", 
+            "Kim Su-an", 
+            "Choi Woo-Shik", 
+            "Ahn So-Hee", 
+            "Kim Eui-Sung", 
+            "etc"
+        ],
+        summary:"",
+        sousGenres:"",
+        cotation:[
+            "IMDb:",
+            "RottenTomatoes:",
+            ""
+        ]
+    },
+    {
+        cover:"",
+        movie:"Conjuring",
+        releaseYear:2013,
+        from:"United States",
+        producer:"James Wan",
+        cast: [
+            "Vera Framiga", 
+            "Patrick Wilson", 
+            "Sterling Jerins", 
+            "Joseph Bishara", 
+            "Shannon Kook", 
+            "Joey King", 
+            "Lili Taylor", 
+            "Lorraine Warren", 
+            "etc"
+        ],
+        summary:"",
+        sousGenres:"",
+        cotation:[
+            "IMDb:",
+            "RottenTomatoes:",
+            ""
+        ]
+    },
+    {
+        cover:"",
+        movie:"Mister Babadook",
+        releaseYear:2014,
+        from:"Australia",
+        producer:"Jennifer Kent",
+        cast:[
+            "Essie Davis", 
+            "Noah Wiseman", 
+            "Daniel Henshall", 
+            "Hayley McElhinney", 
+            "Barbara West", 
+            "Ben Winspear", 
+            "Cathy Adamek", 
+            "Craig Behenna", 
+            "etc"
+        ],
+        summary:"",
+        sousGenres:"",
+        cotation:[
+            "IMDb:",
+            "RottenTomatoes:",
+            ""
+        ]
+    },
+    {
+        cover:"",
+        movie:"Host",
+        releaseYear:2014,
+        from:"Great Britain",
+        producer:"Rob Savage",
+        cast:[
+            "Hayley Bishop", 
+            "Jemma Moore", 
+            "Emma Louise Webb", 
+            "James Swanton", 
+            "Patrick Ward",
+            "Caroline Ward", 
+            "Alan Emrys", 
+            "Seylan Baster", 
+            "etc"
+        ],
+        summary:"",
+        sousGenres:"",
+        cotation:[
+            "IMDb:",
+            "RottenTomatoes:",
+            ""
+        ]
+    },
+    {
+        cover:"",
+        movie:"Insidious",
+        releaseYear:2010,
+        from:"United States",
+        producer:"James Wan",
+        cast: [
+            "Patrick Wilson", 
+            "Leigh Whannell", 
+            "Rose Byrne", 
+            "Ty Simpkins", 
+            "Lin Shaye", 
+            "Angus Sampson", 
+            "Joseph Bishara", 
+            "Andrew Astor",
+            "etc"
+        ],
+        summary:"",
+        sousGenres:"",
+        cotation:[
+            "IMDb:",
+            "RottenTomatoes:",
+            ""
+        ]
+    },
+    {
+        cover:"",
+        movie:"Ju-On",
+        releaseYear: 2000,
+        from:"Japan",
+        producer:"Takashi Shimizu",
+        cast: [
+            "Takako Fuji", 
+            "Ryota Koyama", 
+            "Yûrei Yanagi", 
+            "Chiaki Kuriyama", 
+            "Asumi Miwa", 
+            "Yue", 
+            "Hitomi Miwa", 
+            "Takashi Matsuyama", 
+            "etc"
+        ],
+        summary:"",
+        sousGenres:"",
+        cotation:[
+            "IMDb:",
+            "RottenTomatoes:",
+            ""
+        ]
+    },
+    {
+        cover:"",
+        movie:"Alive",
+        releaseYear:2020,
+        from:"South Korea",
+        producer:"Il Cho",
+        cast: [
+            "Yoo Ah In", 
+            "Park Shin-hye", 
+            "Jeon Bae-Soo", 
+            "Lee Hyun-wook", 
+            "Jin So-Yeon", 
+            "Kim Hak-sun", 
+            "Kim Ra-hee", 
+            "Oh Hye-won", 
+            "etc"
+        ],
+        summary:"",
+        sousGenres:"",
+        cotation:[
+            "IMDb:",
+            "RottenTomatoes:",
+            ""
+        ]
+    },
+    {
+        cover:"",
+        movie:"",
+        releaseYear:"",
+        from:"",
+        producer:"",
+        cast: [
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            ""
+        ],
+        summary:"",
+        sousGenres:"",
+        cotation:[
+            "IMDb:",
+            "RottenTomatoes:",
+            ""
+        ]
+    },
+    {
+        cover:"",
+        movie:"",
+        releaseYear:"",
+        from:"",
+        producer:"",
+        cast: [
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            ""
+        ],
+        summary:"",
+        sousGenres:"",
+        cotation:[
+            "IMDb:",
+            "RottenTomatoes:",
+            ""
+        ]
+    },
+]
+

9f18b12c83dfcd141b0888bf20d18a147c86dfdf
Rosalie <rosalie.boulard@gmail.com>
2023-09-21 17:12:40 +0200

    creating & fixing

--- script.js
+++ script.js
@@ -1,300 +1,260 @@
 const collection = [
     {
-        cover:"",
+        cover:"./assets/covers/boogeyman.jpeg",
         movie:"The BoogeyMan",
         releaseYear:2023,
         from:"United States",
-        producter:"Rob Savage",
-        cast:[
-            "Sophie Thatcher", 
-            "David Dastmalchian", 
-            "Vivien Lyra Blair", 
-            "Chris Messina", 
-            "Madison Hu", 
-            "Marin Ireland", 
-            "Lisa Gay Hamilton", 
-            "Maddie Nichols"
-        ],
-        summary:"",
-        sousGenres:"",
+        producer:"Rob Savage",
+        cast: "Starring: Sophie Thatcher, David Dastmalchian, Vivien Lyra Blair, Chris Messina, Madison Hu, Marin Ireland, Lisa Gay Hamilton, Maddie Nichols",
+        summary:"Summary: A psychiatrist, where a man named Lester Billings talks to the doctor about the 'murders' of his three young children, describing the events of the past several years. His first two children died mysteriously of apparently unrelated causes (diagnosed as crib death and convulsions, respectively) when left alone in their bedrooms. The only commonalities were that the children cried 'Boogeyman!' before being left alone, and the closet door ajar after discovering their corpses, even though Billings is certain the door was shut.",
+        sousGenres:["Mystery", " Thriller"],
         cotation:[
-            "IMDb:",
-            "RottenTomatoes:",
-            ""
-        ]
+            "IMDb:6.0/10",
+            " RottenTomatoes:61%",
+        ],
+        watch: "https://www.youtube.com/watch?v=cFqCmIU0-_M",
     },
     {
-        cover:"",
-        movie:"Crazy Bear",
+        cover:"./assets/covers/crazybeat.jpg",
+        movie:"Cocaine Bear",
         releaseYear:2023,
         from:"United States",
-        producter:"Elizabeth Banks",
-        cast:[
-            "Ray Liotta", 
-            "Keri Russell", 
-            "Jesse Tyler Ferguson", 
-            "Alden Ehrenreich", 
-            "Margot Martindale", 
-            "O'Shea Jackson Jr.", 
-            "Kristofer Hivju", 
-            "Scott Seiss"
-        ],
-        summary:"",
-        sousGenres:"",
+        producer:"Elizabeth Banks",
+        cast: "Starring: Ray Liotta, Keri Russell, Jesse Tyler Ferguson, Alden Ehrenreich, Margot 	Martindale, O'Shea Jackson Jr., Kristofer Hivju, Scott Seiss",
+        summary:"Summary: Inspired by the 1985 true story of a drug runner's plane crash, missing cocaine, and the black bear that ate it, this wild dark comedy finds an oddball group of cops, criminals, tourists and teens converging in a Georgia forest where a 500- pound apex predator has ingested a staggering amount of cocaine and gone on a coke-fueled rampage for more blow... and blood.",
+        sousGenres:"Comedy, Thriller",
         cotation:[
-            "IMDb:",
-            "RottenTomatoes:",
-            ""
-        ]
+            "IMDb:5.9/10",
+            " RottenTomatoes:67%"
+        ],
+        watch: "https://www.youtube.com/watch?v=DuWEEKeJLMI",
     },
     {
-        cover:"",
+        cover:"./assets/covers/blackphone.jpeg",
         movie:"Black Phone",
         releaseYear: 2022,
         from: "United States",
         producer:"Scott Derrickson",
-        cast:[
-            "Ethan Hawke", 
-            "Mason Thames", 
-            "Miguel Cazarez", 
-            "Madelein McGraw", 
-            "Brady Hepner",
-            "Jeremy Davies", 
-            "Tristan Pravong", 
-            "Mike Bailey",
-            "etc"
-        ],
-        summary:"",
-        sousGenres:"",
+        cast:"Starring: Ethan Hawke, Mason Thames, Miguel Cazarez, Madelein McGraw, Brady Hepner, Jeremy Davies, Tristan Pravong, Mike Bailey, and more",
+        summary:"Summary: Finney Blake is a shy but clever 13-year-old boy who is abducted by a sadistic killer and trapped in a soundproof basement where screaming is of no use. When a disconnected phone on the wall begins to ring, Finney discovers that he can hear the voices of the killer's previous victims. And they are dead-set on making sure that what happened to them doesn't happen to Finney.",
+        sousGenres:"Mystery, Thriller",
         cotation:[
-            "IMDb:",
-            "RottenTomatoes:",
-            ""
-        ]
+            "IMDb:6./10",
+            " RottenTomatoes:95%"
+        ],
+        watch: "https://www.youtube.com/watch?v=3eGP6im8AZA",
     },
     {
-        movie:"Last train to Busan",
+        cover:"./assets/covers/Train_to_Busan.jpg",
+        movie:"Train to Busan",
         releaseYear:2016,
         from: "South Korea",
         producer:"Sang-Ho Yeon",
-        cast: [
-            "Gong Yoo", 
-            "Ma Dong-Seok", 
-            "Sang-ho Yeon", 
-            "Jung Yoo Mi", 
-            "Kim Su-an", 
-            "Choi Woo-Shik", 
-            "Ahn So-Hee", 
-            "Kim Eui-Sung", 
-            "etc"
-        ],
-        summary:"",
-        sousGenres:"",
+        cast: "Gong Yoo, Ma Dong-Seok, Sang-ho Yeon, Jung Yoo Mi, Kim Su-an, Choi Woo-Shik, Ahn So-Hee, Kim Eui-Sung, and more",
+        summary:"Summary: Sok-woo, a father with not much time for his daughter, Soo-ahn, are boarding the KTX, a fast train that shall bring them from Seoul to Busan. But during their journey, the apocalypse begins, and most of the earth's population become flesh craving zombies. While the KTX is shooting towards Busan, the passenger's fight for their families and lives against the zombies - and each other.",
+        sousGenres:"Action, Thriller",
         cotation:[
-            "IMDb:",
-            "RottenTomatoes:",
-            ""
-        ]
+            "IMDb:7.6/10",
+            "RottenTomatoes:"
+        ],
+        watch: "https://www.youtube.com/watch?v=pyWuHv2-Abk",
     },
     {
-        cover:"",
+        cover:"./assets/covers/conjuring.jpeg",
         movie:"Conjuring",
         releaseYear:2013,
         from:"United States",
         producer:"James Wan",
-        cast: [
-            "Vera Framiga", 
-            "Patrick Wilson", 
-            "Sterling Jerins", 
-            "Joseph Bishara", 
-            "Shannon Kook", 
-            "Joey King", 
-            "Lili Taylor", 
-            "Lorraine Warren", 
-            "etc"
-        ],
-        summary:"",
-        sousGenres:"",
+        cast: "Vera Framiga, Patrick Wilson, Sterling Jerins, Joseph Bishara, Shannon Kook, Joey King, Lili Taylor, Lorraine Warre, and more",
+        summary:"Summary: In 1971, Carolyn and Roger Perron move their family into a dilapidated Rhode Island farm house and soon strange things start happening around it with escalating nightmarish terror. In desperation, Carolyn contacts the noted paranormal investigators, Ed and Lorraine Warren, to examine the house. What the Warrens discover is a whole area steeped in a satanic haunting that is now targeting the Perron family wherever they go. To stop this evil, the Warrens will have to call upon all their skills and spiritual strength to defeat this spectral menace at its source that threatens to destroy everyone involved.",
+        sousGenres:"Mystery, Thriller",
         cotation:[
-            "IMDb:",
-            "RottenTomatoes:",
-            ""
-        ]
+            "IMDb:7.5/10",
+            " RottenTomatoes:86%"
+        ],
+        watch: "https://www.youtube.com/watch?v=k10ETZ41q5o",
     },
     {
-        cover:"",
+        cover:"./assets/covers/babadook.jpeg",
         movie:"Mister Babadook",
         releaseYear:2014,
         from:"Australia",
         producer:"Jennifer Kent",
-        cast:[
-            "Essie Davis", 
-            "Noah Wiseman", 
-            "Daniel Henshall", 
-            "Hayley McElhinney", 
-            "Barbara West", 
-            "Ben Winspear", 
-            "Cathy Adamek", 
-            "Craig Behenna", 
-            "etc"
-        ],
-        summary:"",
-        sousGenres:"",
+        cast:"Essie Davis, Noah Wiseman, Daniel Henshall, Hayley McElhinney, Barbara West, Ben Winspear, Cathy Adamek, Craig Behenna, and more",
+        summary:"Summary: Amelia, who lost her husband in a car crash on the way to give birth to Samuel, their only child, struggles to cope with her fate as a single mom. Samuel's constant fear of monsters and violent reaction to overcome the fear doesn't help her cause either, which makes her friends become distant. When things can not get any worse, they read a strange book in their house about the 'Babadook' monster that hides in the dark areas of their house. Even Amelia seems to feel the effect of Babadook and desperately tries in vain to destroy the book. The nightmarish experiences the two encounter form the rest of the story.",
+        sousGenres:"Mystery",
         cotation:[
-            "IMDb:",
-            "RottenTomatoes:",
-            ""
-        ]
+            "IMDb:6.8/10",
+            " RottenTomatoes:98%"
+        ],
+        watch: "https://www.youtube.com/watch?v=k5WQZzDRVtw",
     },
     {
-        cover:"",
+        cover:"./assets/covers/Host.jpg",
         movie:"Host",
         releaseYear:2014,
         from:"Great Britain",
         producer:"Rob Savage",
-        cast:[
-            "Hayley Bishop", 
-            "Jemma Moore", 
-            "Emma Louise Webb", 
-            "James Swanton", 
-            "Patrick Ward",
-            "Caroline Ward", 
-            "Alan Emrys", 
-            "Seylan Baster", 
-            "etc"
-        ],
-        summary:"",
-        sousGenres:"",
+        cast:"Hayley Bishop, Jemma Moore, Emma Louise Webb, James Swanton, Patrick Ward, Caroline Ward, Alan Emrys, Seylan Baster, and more",
+        summary:"Summary: Six friends hire a medium to hold a seance via Zoom during lockdown, but they get far more than they bargained for as things quickly go wrong. When an evil spirit starts invading their homes, they begin to realize they might not survive the night.",
+        sousGenres:"Mystery",
         cotation:[
-            "IMDb:",
-            "RottenTomatoes:",
-            ""
-        ]
+            "IMDb:6.5/10",
+            " RottenTomatoes:99%"
+        ],
+        watch: "https://www.youtube.com/watch?v=SNlKbqHqGcY",
     },
     {
-        cover:"",
+        cover:"./assets/covers/insidious.jpg",
         movie:"Insidious",
         releaseYear:2010,
         from:"United States",
         producer:"James Wan",
-        cast: [
-            "Patrick Wilson", 
-            "Leigh Whannell", 
-            "Rose Byrne", 
-            "Ty Simpkins", 
-            "Lin Shaye", 
-            "Angus Sampson", 
-            "Joseph Bishara", 
-            "Andrew Astor",
-            "etc"
-        ],
-        summary:"",
-        sousGenres:"",
+        cast: "Patrick Wilson, Leigh Whannell, Rose Byrne, Ty Simpkins, Lin Shaye, Angus Sampson, Joseph Bishara, Andrew Astor, and more",
+        summary:"Summary: A gripping story of a family in search of help for their son, Dalton, who fell into a coma after a mysterious incident in the attic. Little do they know that there is much more to this endless sleep than meets the eye as they explore the paranormal, and rediscover the past; the key to getting their son back once and for all.",
+        sousGenres:"Mystery, Thriller",
         cotation:[
-            "IMDb:",
-            "RottenTomatoes:",
-            ""
-        ]
+            "IMDb: 6.8/10",
+            " RottenTomatoes:67%"
+        ],
+        watch: "https://www.youtube.com/watch?v=zuZnRUcoWos",
     },
     {
-        cover:"",
+        cover:"./assets/covers/ju_on_the_grudge.jpg",
         movie:"Ju-On",
         releaseYear: 2000,
         from:"Japan",
         producer:"Takashi Shimizu",
-        cast: [
-            "Takako Fuji", 
-            "Ryota Koyama", 
-            "Yûrei Yanagi", 
-            "Chiaki Kuriyama", 
-            "Asumi Miwa", 
-            "Yue", 
-            "Hitomi Miwa", 
-            "Takashi Matsuyama", 
-            "etc"
-        ],
-        summary:"",
-        sousGenres:"",
+        cast: "Takako Fuji, Ryota Koyama, Yûrei Yanagi, Chiaki Kuriyama, Asumi Miwa, Yue, Hitomi Miwa, Takashi Matsuyama, and more",
+        summary:"Summary: n Japan, when the volunteer social assistant Rika Nishina is assigned to visit a family, she is cursed and chased by two revengeful fiends: Kayako, a woman brutally murdered by her husband and her son Toshio. Each person that lives in or visits the haunted house is murdered or disappears.",
+        sousGenres:" ", //none, pure horror movie
         cotation:[
-            "IMDb:",
-            "RottenTomatoes:",
-            ""
-        ]
+            "IMDb:6.7/10",
+            " RottenTomatoes:80%"
+        ],
+        watch: "https://www.youtube.com/watch?v=BxbBdEA7ZCQ",
     },
     {
-        cover:"",
+        cover:"./assets/covers/Alive_2020.jpg",
         movie:"Alive",
         releaseYear:2020,
         from:"South Korea",
         producer:"Il Cho",
-        cast: [
-            "Yoo Ah In", 
-            "Park Shin-hye", 
-            "Jeon Bae-Soo", 
-            "Lee Hyun-wook", 
-            "Jin So-Yeon", 
-            "Kim Hak-sun", 
-            "Kim Ra-hee", 
-            "Oh Hye-won", 
-            "etc"
-        ],
-        summary:"",
-        sousGenres:"",
+        cast: "Yoo Ah In, Park Shin-hye, Jeon Bae-Soo, Lee Hyun-wook, Jin So-Yeon, Kim Hak-sun, Kim Ra-hee, Oh Hye-won, and more",
+        summary:"In Japan, when the volunteer social assistant Rika Nishina is assigned to visit a family, she is cursed and chased by two revengeful fiends: Kayako, a woman brutally murdered by her husband and her son Toshio. Each person that lives in or visits the haunted house is murdered or disappears.",
+        sousGenres:"Drama, Action",
         cotation:[
-            "IMDb:",
-            "RottenTomatoes:",
-            ""
+            "IMDb:6.7/10",
+            " RottenTomatoes:88%"
         ]
     },
     {
-        cover:"",
-        movie:"",
-        releaseYear:"",
-        from:"",
-        producer:"",
-        cast: [
-            "", 
-            "", 
-            "", 
-            "", 
-            "", 
-            "", 
-            "", 
-            "", 
-            ""
-        ],
-        summary:"",
-        sousGenres:"",
+        cover:"./assets/covers/Fear _Street-P1.jpg",
+        movie:"Fear Street Part 1: 1994",
+        releaseYear:"2021",
+        from:"United States",
+        producer:"Leigh Janiak",
+        cast: "Starring: Maya Hawke, Charlene Amoia, David W. Thompson, Noah Bain Garret, Darrell Brutt-Gibson, Ashley Zukerman, Kiana, Madeira, Jana Allen, Benjamin Flores Jr., and more",
+        summary:"Summary: 1994. In Shadyside, the murder capital of the United States, yet another brutal, unmotivated massacre sends shivers down the spine. And, more and more, the unholy name of Sarah Fier, a cursed local witch, keeps popping up. Now, something evil has awoken, forcing a team of five teenage friends to summon up the courage to get to the bottom of a bloody, centuries-old secret and fight tooth and nail against unstoppable supernatural forces to stay alive. But in this dangerous cat-and-mouse game, someone else makes the rules. Can the young defenders lift the town curse and end it all? ",
+        sousGenres:"Drama, Mystery",
         cotation:[
-            "IMDb:",
-            "RottenTomatoes:",
-            ""
-        ]
+            "IMDb:6.2/10",
+            " RottenTomatoes:84%"
+        ],
+        watch: "https://www.youtube.com/watch?v=e69MvWv3ffY",
     },
     {
-        cover:"",
-        movie:"",
-        releaseYear:"",
-        from:"",
-        producer:"",
-        cast: [
-            "", 
-            "", 
-            "", 
-            "", 
-            "", 
-            "", 
-            "", 
-            "", 
-            ""
-        ],
-        summary:"",
-        sousGenres:"",
+        cover:"./assets/covers/Nope.jpg",
+        movie:"Nope",
+        releaseYear:"2022",
+        from:"United States",
+        producer:"Jordan Peele",
+        cast: "Starring: Daniel Kaluuya, Keke Palmer, Brandon Perea, Michael Wincott, Steven Yeun, Wrenn Schmidt, Keith David, Devon Grave, and more",
+        summary:"Summary: After random objects falling from the sky result in the death of their father, ranch-owning siblings OJ and Emerald Haywood attempt to capture video evidence of an unidentified flying object with the help of tech salesman Angel Torres and documentarian Antlers Holst.",
+        sousGenres:"Mystery, Science-Fiction",
         cotation:[
-            "IMDb:",
-            "RottenTomatoes:",
-            ""
-        ]
+            "IMDb:6.8/10",
+            " RottenTomatoes:83%"
+        ],
+        watch:"https://www.youtube.com/watch?v=In8fuzj3gck",
     },
 ]
 
+
+collection.forEach((object) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    document.querySelector('.myCollection').appendChild(card)
+    
+    const cover = document.createElement('img');
+    cover.classList.add('cover');
+    cover.src=object.cover;
+    card.appendChild(cover);
+
+    const genre=document.createElement('div');
+    genre.classList.add('genre');
+    card.appendChild(genre);
+
+    const sousGenre=document.createElement('h2');
+    sousGenre.textContent=object.sousGenres;
+    genre.appendChild(sousGenre);
+
+    const info=document.createElement('div');
+    info.classList.add('info')
+    card.appendChild(info);
+
+    const h3=document.createElement('h3');
+    h3.textContent=object.producer;
+    info.appendChild(h3);
+
+    const table=document.createElement('div');
+    table.classList.add('table');
+    h3.appendChild(table);
+
+    const releaseYear=document.createElement('p')
+    releaseYear.classList.add('releaseYear');
+    releaseYear.textContent=object.releaseYear;
+    table.appendChild(releaseYear);
+
+    const from=document.createElement('p');
+    from.classList.add('from');
+    from.textContent=object.from;
+    table.appendChild(from);
+
+    const about=document.createElement('div');
+    about.classList.add('about');
+    card.appendChild(about);
+
+    const cast=document.createElement('p');
+    cast.textContent=object.cast;
+    about.appendChild(cast);
+
+    const summary=document.createElement('p');
+    summary.textContent=object.summary;
+    about.appendChild(summary);
+
+    const end=document.createElement('div');
+    end.classList.add('end');
+    card.appendChild(end);
+
+    const cotation=document.createElement('p');
+    cotation.textContent=object.cotation;
+    end.appendChild(cotation);
+
+    const trailer=document.createElement('div');
+    trailer.classList.add('trailer');
+    card.appendChild(trailer);
+
+    const watch=document.createElement('a');
+    watch.href=object.watch;
+    trailer.appendChild(watch);
+
+    const logo=document.createElement('img');
+    logo.classList.add('logo');
+    logo.src='./assets/logo/youtube.png';
+    watch.appendChild(logo);
+
+
+    return object;
+     
+});
